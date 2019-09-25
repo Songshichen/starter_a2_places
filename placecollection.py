c61a0333481d6f6fed9a33ebf79741291ae4e75f
Song <54587181+Songshichen@users.noreply.github.com>
2019-09-25 17:41:44 +0800

    Update placecollection.py

--- placecollection.py
+++ placecollection.py
@@ -1,9 +1,17 @@
-"""..."""
-
-
-# Create your PlaceCollection class in this file
+from place import Place
 
 
 class PlaceCollection:
-    """..."""
-    pass
+    def __init__(self):
+        """
+            This function is to initialize an empty list for Place object
+        """
+        self.places = []
+
+    def get_places(self,name):
+        """
+            This function used the method to return a selected single place object by user.
+        """
+        for place in self.places:
+            if place[0].name == name:
+                return place[0]
