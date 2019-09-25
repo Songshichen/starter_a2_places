5852b055b20c9d816bbccff91382194bbd65ee03
Song <54587181+Songshichen@users.noreply.github.com>
2019-09-25 17:40:53 +0800

    Update place.py

--- place.py
+++ place.py
@@ -1,9 +1,16 @@
-"""..."""
-
-
-# Create your Place class in this file
-
 
 class Place:
-    """..."""
-    pass
+    def __init__(self, name, country, priority, status):
+        """
+            Set the name, name, priority and status of each item to self.**** on initialisation
+        """
+        self.name = name
+        self.country = country
+        self.priority = priority
+        self.status = status
+
+    def mark_song(self, status):
+        """
+            Used to mark the song status
+        """
+        self.status = status
